bbde648404293c1d171c53b000faf17f0be2f4c8
unknown <marcusgohkz@gmail.com>
2025-06-30 16:34:21 +0800

    minor changes to build file

--- app/src/main/java/org/todobot/Parsers/HelpParser.java
+++ app/src/main/java/org/todobot/Parsers/HelpParser.java
@@ -5,7 +5,6 @@ import org.todobot.common.CommandType;
 public class HelpParser extends CommandParser {
     @Override
     public ParseResult parse(String arguments) {
-        // Help command doesn't need arguments - maps to canonical "help" command
         return new ParseResult(CommandType.HELP, new String[0]);
     }
     
