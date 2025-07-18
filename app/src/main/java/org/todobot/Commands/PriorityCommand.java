711761d902fa5eadd989fe12aaca27f12860a836
unknown <marcusgohkz@gmail.com>
2025-07-18 12:44:09 +0800

    Priority command class now returns ParserResult

--- app/src/main/java/org/todobot/Commands/PriorityCommand.java
+++ app/src/main/java/org/todobot/Commands/PriorityCommand.java
@@ -3,6 +3,7 @@ package org.todobot.commands;
 import org.todobot.common.BotMessages;
 import org.todobot.model.Priority;
 import org.todobot.model.Task;
+import org.todobot.parsers.ParseResult;
 import org.todobot.service.TaskList;
 
 public class PriorityCommand extends Command {
@@ -13,6 +14,12 @@ public class PriorityCommand extends Command {
 
     @Override
     public String execute(String[] arguments) {
+        throw new UnsupportedOperationException("PriorityCommand requires a ParseResult object.");
+    }
+
+    @Override
+    public String execute(ParseResult parseResult) {
+        String[] arguments = parseResult.getArguments();
         int taskNumber = Integer.parseInt(arguments[0]);
         Task task = taskList.getTask(taskNumber);
         if (task == null) return BotMessages.INVALID_TASK_NUMBER;
